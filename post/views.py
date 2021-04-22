415888f891d01da2eb93b7fb86cd8c521e6868ab
Dzhanbekov <mukhamed.dzhanbekov@mail.ru>
2021-04-22 17:12:49 +0600

    begin

---
+++ post/views.py
@@ -0,0 +1,34 @@
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Post
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
+
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+    template_name = 'post_edit.html'
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
